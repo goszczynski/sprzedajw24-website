a445513c898cb4e15d3b64d93a346fdfa0196849
Piotr Goszczyński <piotrgoszczynski@Piotrs-MacBook-Pro.local>
2022-11-20 12:04:37 +0100

    site oferta is done

--- js/script.js
+++ js/script.js
@@ -6,3 +6,19 @@
         menu.classList.toggle('is-open');
     });
 })();
+
+
+var acc = document.getElementsByClassName("questions__list");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var panel = this.nextElementSibling;
+    if (panel.style.maxHeight) {
+      panel.style.maxHeight = null;
+    } else {
+      panel.style.maxHeight = panel.scrollHeight + "px";
+    }
+  });
+}
